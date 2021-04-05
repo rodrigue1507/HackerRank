85d2530382f2964e68d37dd720c675e3888a313b
penifana ouattara <peniouatt@hotmail.fr>
2021-04-05 17:21:51 +0200

    kata Valleys

---
+++ HackerRankChallenge/SalesByMatchWithLinqQuery.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HackerRankChallenge
+{
+    public class SalesByMatchWithLinqQuery
+    {
+        public int SockMerchant(int[] ar)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ HackerRankChallenge/Valleys.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace HackerRankChallenge
+{
+    public class Valleys
+    {
+        public int CountingValleys(string path)
+        {
+            bool seaLevel = true;
+            int down = 0;
+            int up = 0;
+            int mountain = 0;
+            int valley = 0;
+            bool isUphill = false;
+            foreach (var level in path.ToUpper())
+            {
+                if (seaLevel)
+                {
+                    if (level == 'U')
+                    {
+                        isUphill = true;
+                    }
+                    else
+                    {
+                        isUphill = false;
+                    }
+                    seaLevel = false;
+                }
+                if (level == 'U')
+                {
+                    up++;
+                }
+                else
+                {
+                    down++;
+                }
+                if (up == down)
+                {
+                    if(isUphill == true)
+                    {
+                        mountain++;
+                    }
+                    else
+                    {
+                        valley++;
+                    }
+                    seaLevel = true;
+                }
+            }
+            return valley;
+        }
+    }
+}
