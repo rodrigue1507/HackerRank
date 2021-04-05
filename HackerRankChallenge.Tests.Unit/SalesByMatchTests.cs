2ae4f2ca3758fe377d602390fe106419d5efe4d1
penifana ouattara <peniouatt@hotmail.fr>
2021-04-05 12:56:06 +0200

    SalesMatch Avec un dictionnaire

--- HackerRankChallenge.Tests.Unit/SalesByMatchTests.cs
+++ HackerRankChallenge.Tests.Unit/SalesByMatchTests.cs
@@ -8,11 +8,14 @@ namespace HackerRankChallenge.Tests.Unit
     {
 
         //[TestCase(7,new int[] {1,2,1,2,1,3,2})]
-        [TestCase(9,new int[] { 10,20,20,10,10,30,50,10,20})]
-        public void GiveTotalPairsSocks(int n,int[] ar)
+        [TestCase(new int[] { 10,20,20,10,10,30,50,10,20})]
+        public void GiveTotalPairsSocks(int[] ar)
         {
-            SalesByMatch salesByMatch = new SalesByMatch();
-            int totalPair = salesByMatch.SockMerchant(n,ar);
+            //SalesByMatch salesByMatch = new SalesByMatch();
+            //int totalPair = salesByMatch.SockMerchant(n,ar);
+            //totalPair.Should().Be(3);
+            SalesByMatchWithDictionary salesByMatchDic = new SalesByMatchWithDictionary();
+            int totalPair = salesByMatchDic.SockMerchant( ar);
             totalPair.Should().Be(3);
         }
     }
---
+++ HackerRankChallenge/SalesByMatchWithDictionary.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+
+namespace HackerRankChallenge
+{
+    public class SalesByMatchWithDictionary
+    {
+        public int SockMerchant(int[] ar)
+        {
+            var dic = new Dictionary<int,int>();
+            int result = 0;
+            for (int i = 0; i < ar.Length; i++)
+            {
+                if (!dic.ContainsKey(ar[i]))
+                {
+                    dic.Add(ar[i],1);
+                }
+                else
+                {
+                    dic[ar[i]]++;
+                }       
+            }
+            foreach (var key  in dic.Keys)
+            {
+                result += dic[key]/2;
+            }
+            return result;
+        }
+    }
+}
